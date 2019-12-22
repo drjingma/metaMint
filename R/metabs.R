c7407800be7a99132c844bda73b224e99950c684
Jing Ma <crystal.jing.ma@gmail.com>
2019-12-22 16:56:32 +0000

    add metabs data

---
+++ R/metabs.R
@@ -0,0 +1,10 @@
+#' Metabolite data
+#'
+#' @name metabs
+#' @docType data
+#' @author Jing Ma (\email{jingma@@fredhutch.org})
+#' @references \url{https://www.nature.com/articles/srep14174}
+#' @keywords data
+#' @format A 128 by 131 data matrix of metabolite concentration profiles.
+#' @description This data matrix consists of the concentration profiles from 128 metabolites.
+NULL
