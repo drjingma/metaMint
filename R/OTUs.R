f420bfd8935845cc8f414ae2a8445b044dd01fe6
Jing Ma <crystal.jing.ma@gmail.com>
2019-12-22 16:56:20 +0000

    add OTUs data

---
+++ R/OTUs.R
@@ -0,0 +1,10 @@
+#' OTU count data
+#'
+#' @name OTUs
+#' @docType data
+#' @author Jing Ma (\email{jingma@@fredhutch.org})
+#' @references \url{https://www.nature.com/articles/srep14174}
+#' @keywords data
+#' @format A 51 by 131 data matrix of OTU counts.
+#' @description This data matrix consists of the sequencing counts from 51 bacterial taxa. 
+NULL
